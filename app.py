218156a02e6c5d02e7b2b29f13820078a984ebf8
amanbhatt97 <amanbhatt.1997.ab@gmail.com>
2023-08-25 13:15:40 +0530

    added predict probability button

--- app.py
+++ app.py
@@ -81,27 +81,27 @@ with col2:
         wickets = st.number_input('Wickets out', step = 1, min_value=0, max_value=9)
 
 
-    # #----- make a button for 'Predict Probability' ------#
-    # if st.button('Predict Probability'):
+    #----- make a button for 'Predict Probability' ------#
+    if st.button('Predict Probability'):
             
-    runs_left = target - score
-    balls_left = 120 - (overs * 6)
-    wickets_left = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left * 6)/balls_left
+        runs_left = target - score
+        balls_left = 120 - (overs * 6)
+        wickets_left = 10 - wickets
+        crr = score/overs
+        rrr = (runs_left * 6)/balls_left
 
-    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],
-                            'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],
-                            'wickets_remain':[wickets_left],'target':[target],'crr':[crr],'rrr':[rrr]})
+        input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],
+                                'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],
+                                'wickets_remain':[wickets_left],'target':[target],'crr':[crr],'rrr':[rrr]})
 
-    result = pipe.predict_proba(input_df)
-    loss = result[0][0]
-    win = result[0][1]
+        result = pipe.predict_proba(input_df)
+        loss = result[0][0]
+        win = result[0][1]
 
-    st.markdown("---")
-    st.header('Winning Probability') 
-    st.info(batting_team + " - " + str(round(win*100)) + "%")
-    st.info(bowling_team + " - " + str(round(loss*100)) + "%")
+        st.markdown("---")
+        st.header('Winning Probability') 
+        st.info(batting_team + " - " + str(round(win*100)) + "%")
+        st.info(bowling_team + " - " + str(round(loss*100)) + "%")
 
 
 
